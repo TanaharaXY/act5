2f78e557e59518abd3f2d36d810b264b28cf000b
Alfredo <alfredotanahara@hotmail.com>
2020-09-10 20:40:10 -0500

    act5

---
+++ src/operacion/aritmetica.java
@@ -0,0 +1,24 @@
+package operacion;
+
+public class aritmetica {
+    public static void main(String ...args){
+
+        int x, y, suma, resta, multi, divi, mod;
+
+        x = 12;
+        y = 4;
+        suma = x + y;
+        resta = x - y;
+        multi = x * y;
+        divi = x / y;
+        mod = x % y;
+
+        System.out.println("Actividad 5 Alfredo Lara");
+        System.out.println("La suma es: " + suma);
+        System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicacion es: " + multi);
+        System.out.println("La division: " + divi);
+        System.out.println("El mod es: " + mod);
+
+    }
+}
